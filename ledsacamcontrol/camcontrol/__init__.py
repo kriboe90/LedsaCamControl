d61e475340435bc0b64732ac464701a7db7bb44b
kriboe90 <boerger.k@icloud.com>
2021-04-22 10:19:12 +0200

    adding camera config to init

--- ledsacamcontrol/camcontrol/__init__.py
+++ ledsacamcontrol/camcontrol/__init__.py
@@ -1,1 +1,1 @@
-from .camcontrol import capture_image_at_shutter_speed, get_shutter_speed_list
+from .camcontrol import capture_image_at_shutter_speed, get_shutter_speed_list, set_config, show_config
